76839635dab7487b4dfbe126060ecdcc8f4691aa
Tanaxeri <tamasgamer@gmail.hu>
2023-01-16 14:06:13 +0100

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace DinamikusForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tarolo = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // tarolo
+            // 
+            this.tarolo.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tarolo.Location = new System.Drawing.Point(530, 0);
+            this.tarolo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tarolo.Name = "tarolo";
+            this.tarolo.Size = new System.Drawing.Size(670, 658);
+            this.tarolo.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 658);
+            this.Controls.Add(this.tarolo);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel tarolo;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinamikusForm
+{
+
+    public partial class MainForm : Form
+    {
+
+        int sorokSzama = 3;
+        int oszlopokSzama = 6;
+        int buttonSize = 60;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < sorokSzama; i++)
+            {
+
+                for (int j = 0; j < oszlopokSzama; j++)
+                {
+
+                    Button button = new Button();
+                    button.SetBounds(j * buttonSize, i * buttonSize, buttonSize, buttonSize);
+                    button.Text = (i * oszlopokSzama + j).ToString();
+                    button.Click += (o, ev)=>{
+
+                        Button gomb = (Button)o;
+                        MessageBox.Show(gomb.Text);
+                        gomb.BackColor = Color.Green;
+
+                    }; 
+                    tarolo.Controls.Add(button);
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
